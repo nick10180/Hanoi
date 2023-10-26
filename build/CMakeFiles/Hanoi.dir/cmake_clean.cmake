fe94b457d88844c8006000df53b4a5d7616d1bb1
Nick <nick@antinick>
2023-10-26 15:57:13 -0400

    >Ugly commit because VSCode hates me

---
+++ build/CMakeFiles/Hanoi.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hanoi.dir/main.cpp.o"
+  "Hanoi"
+  "Hanoi.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Hanoi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
